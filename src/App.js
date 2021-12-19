ca88c0e2365a2d4b1595a93e4329eea2f7ed2197
Mar.io <mario@stake.co.jp>
2021-12-19 23:11:20 +0100

    added path

--- src/App.js
+++ src/App.js
@@ -199,13 +199,13 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        image={CONFIG.SHOW_BACKGROUND ? "/shwidelcome-nft/config/images/bg.png" : null}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        <StyledLogo alt={"logo"} src={"/shwidelcome-nft/config/images/logo.png"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"example"} src={"/shwidelcome-nft/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
